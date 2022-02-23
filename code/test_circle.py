aea826f89339baf0d57d354d3bb462b83bfd9b92
Kiana <1436789500@qq.com>
2022-02-23 22:23:00 +0800

    绘制外接椭圆并点上圆心

--- code/test_circle.py
+++ code/test_circle.py
@@ -11,18 +11,29 @@ for row in range(h):
         if img[col, row] == 255:
             temp = np.array([row, col])  # 注意这儿数组与图像坐标是反过来的？
             vec = np.c_[vec, temp]
-vec = np.transpose(vec)
-vec = np.delete(vec, 0, 0)
+vec = np.transpose(vec)  # 转置一下
+vec = np.delete(vec, 0, 0)  # 获取轮廓点集合
 
 # 寻找凸包并绘制凸包（轮廓）
 hull = cv2.convexHull(vec)
 hull = np.squeeze(hull)  # 去除多维数组中，维度为1的维度
 print(hull)
 
-length = len(hull)
-for i in range(len(hull)-1):
-    # cv2.circle(img, (hull[i][1], hull[i][0]), radius=1, color=255, thickness=2)
-    cv2.line(img, hull[i], hull[i+1], color=255, thickness=2)
+# 绘制凸包
+# length = len(hull)
+# for i in range(len(hull)-1):
+#     # cv2.circle(img, (hull[i][1], hull[i][0]), radius=1, color=255, thickness=2)
+#     cv2.line(img, hull[i], hull[i+1], color=255, thickness=2)
+
+# 绘制外接椭圆
+ellipse = cv2.fitEllipse(hull)
+[(x, y), (a, b), angle] = cv2.fitEllipse(hull)
+img = cv2.ellipse(img, ellipse, 255, 2)
+print("中心坐标：%f,%f", x, y)
+print("长轴：%f；短轴%f", a, b)
+print("旋转角度：%f", angle)
+cv2.circle(img, (round(x), round(y)), radius=1, color=255, thickness=2)
+
 
 # 显示图片
 cv2.imshow('img', img)
